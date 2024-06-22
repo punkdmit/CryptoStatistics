239b213f1badaa4f11de119212d72390a7070610
punkdmit <71826735+punkdmit@users.noreply.github.com>
2024-06-22 20:07:16 +0300

    Project config: - pods (SnapKit, SwiftLint) installed. - Removed StoryBoards

---
+++ CryptoStatistics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoStatistics
+//
+//  Created by Dmitry Apenko on 22.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+def shared_pods
+	pod 'SnapKit'
+	pod 'SwiftLint'
+end
+
+target 'CryptoStatistics' do
+
+ 	use_frameworks!
+	shared_pods
+
+end
